dd91aa7e84b55db363e076d6bdbb528ca5e179bd
jack <jack@gmail.com>
2024-11-28 15:29:20 +0800

    week12

---
+++ week12/week12-2.cpp
@@ -0,0 +1,7 @@
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //ListNode* ans =list1;//宣告答案，接到list1
+        //ListNode* ans =new ListNode(99);//新增值是99
+        ///ListNode* ans=new ListNode(99,new ListNode(88));
+		return ans;
+	}
+};
